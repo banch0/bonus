961c3784bd12118d99d9cd89e028262c8ecdb692
Behruz Valiev <behruzvaliev@gmail.com>
2020-02-05 17:14:12 +0500

    fix: fixes #5

--- main.go
+++ main.go
@@ -2,13 +2,15 @@ package main
 
 func main() {}
 
-func bonus(sale int) int {
+func bonus(sales []int) int {
 	minSaleValue := 10_000
-	if sale >= minSaleValue {
-		percent := 100
-		bonusValue := 5
-		managerBonus := (sale / percent) * bonusValue
-		return managerBonus
+	for _, sale := range sales {
+		if sale >= minSaleValue {
+			percent := 100
+			bonusValue := 5
+			managerBonus := (sale / percent) * bonusValue
+			return managerBonus
+		}
 	}
 	return 0
 }
